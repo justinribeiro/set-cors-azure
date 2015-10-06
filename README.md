f97c816433ce63a5cbdf38d32c62d7ec310d0e7a
Justin Ribeiro <justin@justinribeiro.com>
2015-10-06 13:55:37 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # set-cors-azure
 Command line tool in C# for Windows that enables CORS for Azure Blob Store / CDN
+
+## Basics
+
+1. Open in Visual Studio.
+
+2. Make sure you install appropraite libs vi Nuget (ala, Azure Storage API).
+
+3. Build command line tool.
+
+4. Use command line tool with Storage Credentials:
+
+```setCors.exe accountName keyValue```
+
---
+++ setCors/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Shared.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace setCors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StorageCredentials storageCredentials = new StorageCredentials(args[0], args[1]);
+            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Define our basics for quick op
+            ServiceProperties blobServiceProperties = new ServiceProperties()
+            {
+                HourMetrics = null,
+                MinuteMetrics = null,
+                Logging = null,
+            };
+
+            // Define our CORS rules (wide open here)
+            CorsRule corsRule = new CorsRule()
+            {
+                AllowedHeaders = new List<string>() { "*" },
+                AllowedMethods = CorsHttpMethods.Get | CorsHttpMethods.Post | CorsHttpMethods.Head | CorsHttpMethods.Put,
+                AllowedOrigins = new List<string>() { "*" },
+                ExposedHeaders = new List<string>() { "*", "Accept-Ranges", "Content-Range"},
+                
+                MaxAgeInSeconds = 7200
+            };
+
+            // Set our rule
+            blobServiceProperties.Cors.CorsRules.Add(corsRule);
+
+            try 
+            { 
+                blobClient.SetServiceProperties(blobServiceProperties);
+                Console.Out.WriteLine("CORS is rollin' for " + args[0]);
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.ToString());
+            }
+        }
+    }
+}
